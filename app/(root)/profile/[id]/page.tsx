9c1e519f004f01818ce10df95627133f083dbdaf
vasanth2320 <vasanthkumars2025@gmail.com>
2023-11-10 12:01:52 +0530

    created profileheader for profile page

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,38 @@
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+const Page = async ({ params }: { params: { id: string } }) => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  return (
+    <section>
+      <ProfileHeader
+        accountId={userInfo.id}
+        authUserId={user.id}
+        name={userInfo.name}
+        username={userInfo.username}
+        imgUrl={userInfo.image}
+        bio={userInfo.bio}
+      />
+
+      <div className="mt-9">
+        <Tabs defaultValue="threads" className="w-full">
+          <TabsList className="tab">
+            
+          </TabsList>
+        </Tabs>
+      </div>
+    </section>
+  );
+};
+
+export default Page;
