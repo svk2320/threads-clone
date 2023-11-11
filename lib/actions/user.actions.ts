7cefa88603e173b7d0face3caa6e9cfc919d72b4
vasanth2320 <vasanthkumars2025@gmail.com>
2023-11-11 09:35:51 +0530

    created activities page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -92,14 +92,14 @@ export async function fetchUsers({
   searchString = "",
   pageNumber = 1,
   pageSize = 20,
-  sortBy = "desc"
- } : {
+  sortBy = "desc",
+}: {
   userId: string;
   searchString: string;
   pageNumber?: number;
   pageSize?: number;
   sortBy?: SortOrder;
- }) {
+}) {
   try {
     connectToDB();
 
@@ -108,22 +108,22 @@ export async function fetchUsers({
     const regex = new RegExp(searchString, "i");
 
     const query: FilterQuery<typeof User> = {
-      id: { $ne: userId}
-    }
+      id: { $ne: userId },
+    };
 
-    if(searchString.trim() !== '') {
+    if (searchString.trim() !== "") {
       query.$or = [
-        { username: { $regex: regex} },
-        { name: { $regex: regex} }
-      ]
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
     }
 
     const sortOptions = { createdAt: sortBy };
 
     const usersQuery = User.find(query)
-                           .sort(sortOptions)
-                           .skip(skipAmount)
-                           .limit(pageSize);
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
 
     const totalUsersCount = await User.countDocuments(query);
 
@@ -131,8 +131,35 @@ export async function fetchUsers({
 
     const isNext = totalUsersCount > skipAmount + users.length;
 
-    return { users, isNext};
+    return { users, isNext };
   } catch (error: any) {
     throw new Error(`Failed to fetch users: ${error.message}`);
+  }
+}
+
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads created by the user
+    const userThreads = await Thread.find({ author: userId });
+
+    // Collect all the child thread ids (replies) from the 'children' field
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch activity: ${error.message} `);
   }
 }
